f5f55f53352d5dc2328d3f3933ce25bdc12a8155
Alexander F. Rødseth <rodseth@gmail.com>
2026-02-26 14:30:52 +0100

    Update the rtaudio example

--- examples/rtaudio/main.cpp
+++ examples/rtaudio/main.cpp
@@ -1,11 +1,11 @@
-// Example from the RtAudio documentation
+// Example from the RtAudio documentation (updated for RtAudio 6)
 
-#include "RtAudio.h"
+#include <rtaudio/RtAudio.h>
 #include <cstdlib>
 #include <iostream>
 
 // Two-channel sawtooth wave generator.
-int saw(void* outputBuffer, void* inputBuffer, unsigned int nBufferFrames, double streamTime,
+int saw(void* outputBuffer, void* /*inputBuffer*/, unsigned int nBufferFrames, double /*streamTime*/,
     RtAudioStreamStatus status, void* userData)
 {
     unsigned int i, j;
@@ -14,7 +14,6 @@ int saw(void* outputBuffer, void* inputBuffer, unsigned int nBufferFrames, doubl
     if (status)
         std::cout << "Stream underflow detected!" << std::endl;
 
-    // Write interleaved audio data.
     for (i = 0; i < nBufferFrames; i++) {
         for (j = 0; j < 2; j++) {
             *buffer++ = lastValues[j];
@@ -32,33 +31,31 @@ int main()
     RtAudio dac;
     if (dac.getDeviceCount() < 1) {
         std::cout << "\nNo audio devices found!\n";
-        exit(0);
+        return 0;
     }
     RtAudio::StreamParameters parameters;
     parameters.deviceId = dac.getDefaultOutputDevice();
     parameters.nChannels = 2;
     parameters.firstChannel = 0;
     unsigned int sampleRate = 44100;
-    unsigned int bufferFrames = 256; // 256 sample frames
-    double data[2];
-    try {
-        dac.openStream(
-            &parameters, NULL, RTAUDIO_FLOAT64, sampleRate, &bufferFrames, &saw, (void*)&data);
-        dac.startStream();
-    } catch (RtAudioError& e) {
-        e.printMessage();
-        exit(0);
+    unsigned int bufferFrames = 256;
+    double data[2] = {};
+
+    if (dac.openStream(&parameters, nullptr, RTAUDIO_FLOAT64, sampleRate, &bufferFrames, &saw, (void*)&data) != RTAUDIO_NO_ERROR) {
+        std::cerr << "Failed to open stream\n";
+        return 1;
+    }
+    if (dac.startStream() != RTAUDIO_NO_ERROR) {
+        std::cerr << "Failed to start stream\n";
+        return 1;
     }
 
     char input;
     std::cout << "\nPlaying ... press <enter> to quit.\n";
     std::cin.get(input);
-    try {
-        // Stop the stream
+
+    if (dac.isStreamRunning())
         dac.stopStream();
-    } catch (RtAudioError& e) {
-        e.printMessage();
-    }
     if (dac.isStreamOpen())
         dac.closeStream();
     return 0;
