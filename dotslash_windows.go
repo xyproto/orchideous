befb81c32230f3793eeef0fba9bf2032299cfedd
Alexander F. Rødseth <rodseth@gmail.com>
2026-02-27 10:55:16 +0100

    Minor fix

--- dotslash_windows.go
+++ dotslash_windows.go
@@ -9,7 +9,7 @@ import (
 
 // dotSlash prepends ./ to a relative path to make it executable.
 func dotSlash(name string) string {
-	if filepath.IsAbs(name) || strings.HasPrefix(name, "./") || strings.HasPrefix(name, ".\\") {
+	if filepath.IsAbs(name) || strings.HasPrefix(name, "./") || strings.HasPrefix(name, ".\\") || strings.HasPrefix(name, "/") {
 		return name
 	}
 	return "./" + name
